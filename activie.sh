8c767ff88a58b202ed503e74d5f4e36ec5c27e96
ngan_ct <caothuongngan@gmail.com>
2023-07-10 02:42:18 +0700

    commit

---
+++ activie.sh
@@ -0,0 +1,3 @@
+for (( i = 0; i < 3; i++ )); do
+    php artisan dusk
+done
--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,36 +2,22 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
     public function testExample()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('https://accounts.google.com/v3/signin/identifier?dsh=S938677%3A1688717001142761&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ifkv=AeDOFXiYzOKVYILWicKKICFnrMgtpCSPlubKEP6A1y_gkoafge1OwdB090oEzPJbcwnHKor2poUP&osid=1&passive=1209600&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-                ->type('identifier', 'caothuongngan@gmail.com')
-//                ->type('password', 'test1234')
-                ->press('Next');
-            sleep(100);
+        $jsonFilePath = storage_path('app/cookies.json');
+        $cookies = json_decode(file_get_contents($jsonFilePath), true);
+        $this->browse(function (Browser $browser) use ($cookies) {
+            $browser->visit('https://mail.google.com/mail/u/0/#inbox');
+            foreach ($cookies as $cookie) {
+                $browser->driver->manage()->addCookie($cookie);
+            }
+            $browser->driver->navigate()->refresh();
+            sleep(10);
         });
-//        for ($i = 0; $i < 10; $i++) {
-//            $this->browse(function (Browser $browser) use ($i) {
-//                $browser->visit('https://accounts.google.com/v3/signin/identifier?dsh=S-250611689%3A1688709069015982&continue=https%3A%2F%2Fads.google.com%2Fnav%2Flogin&flowName=GlifWebSignIn&flowEntry=AddSession')
-//                    ->type('email', 'admin@gmail.com')
-//                    ->type('password', '111111')
-//                    ->press('Login');
-//
-//                sleep(1);
-//            });
-//            sleep(1);
-//        }
     }
 }
